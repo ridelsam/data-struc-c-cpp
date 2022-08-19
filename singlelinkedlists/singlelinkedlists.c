6abbc940919ce82240ac998a4ff6ef089b7411ef
Ridel Samonte <ridel.asam@gmail.com>
2022-08-18 19:49:39 -0700

    deleting certain node position/entire list and reversing list

--- singlelinkedlists/singlelinkedlists.c
+++ singlelinkedlists/singlelinkedlists.c
@@ -1,40 +1,24 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
-struct node {
-    int data;
-    struct node *link;
-};
+#include "singleLLfunc.c"
 
 
-void add_at_end(struct node *head, int data);
-void add_at_beg(struct node **head, int data);
-void add_at_pos(struct node *head, int data, int pos);
-struct node* del_first(struct node *head);
-void del_last(struct node *head);
+
+
 
 int main(){
 
-    //1st node
+    //Creating single linked list
     struct node *head = malloc(sizeof(struct node));
     head->data = 45;
     head->link = NULL;
-
   
     add_at_end(head, 98);
     add_at_end(head, 3);
     add_at_end(head, 67);
 
-    
-
-    //insering node at a certain position
-    int data = 50, pos = 3;
-
-    add_at_pos(head, data, pos);
     struct node *ptr = head;
-
-
 
     //print data 
     printf("Original Linked list:   ");
@@ -45,17 +29,18 @@ int main(){
 
     }
 
-    del_last(head);
-    ptr=head;
+    head = reverse(head);
+    ptr = head;
 
     //list after node deletion
-    printf("\nlist after deletion:    ");
+    printf("\nlist after reversal:    ");
     while(ptr != NULL)
     {
         printf("%d  ", ptr->data);
         ptr = ptr->link;
 
     }
+    
 
     return 0;
 
@@ -63,82 +48,3 @@ int main(){
 
 
 
-void add_at_end(struct node *head, int data){
-    struct node *ptr = head;
-    struct node *temp = (struct node*)malloc(sizeof(struct node));
-    temp->data = data;
-    temp->link = NULL;
-    
-    while(ptr->link != NULL){
-        ptr = ptr->link;
-    }
-
-    ptr->link = temp;
-    
-
-}
-
-void add_at_beg(struct node **head, int data){
-     struct node *ptr = (struct node*)malloc(sizeof(struct node)); 
-     ptr->data = data;   
-     ptr->link = NULL;
-
-     ptr->link = *head;
-     *head = ptr;
-      
-}
-
-void add_at_pos(struct node *head, int data, int pos){
-    struct node *ptr = head;
-    struct node *ptr2 = (struct node*)malloc(sizeof(struct node)); 
-    ptr2->data = data;
-    ptr2->link = NULL;
-
-    pos--;
-    while(pos != 1)
-    {
-        ptr = ptr->link;
-        pos--;
-    }
-
-    ptr2->link = ptr->link;
-    ptr->link = ptr2;
-
-}
-
-struct node* del_first(struct node *head){
-
-    if(head == NULL)
-        printf("List is already empty!");
-    else
-    {
-        struct node *temp = head;
-        head = head->link;
-        free(temp);
-    }
-
-    return head;
-
-}
-
-void del_last(struct node *head){
-    if(head == NULL)
-        printf("List is already empty!");
-    else if(head->link == NULL)
-    {
-        free(head);
-        head = NULL;
-    }
-    else 
-    {
-        struct node *temp = head;
-        while(temp->link->link != NULL)
-        {
-            temp = temp->link;
-        }
-        free(temp->link);
-        temp->link = NULL;
-    }
-
-
-}
