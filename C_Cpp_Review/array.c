61ace78a809f5dbae45394f46316a4f9dc2a25c0
Ridel Samonte <ridel.asam@gmail.com>
2022-09-03 21:59:39 -0700

    first commit; c and c++ review

---
+++ C_Cpp_Review/array.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(){
+
+
+int arr[5];
+
+int arr_b[5] = {2, 4, 6, 8, 10};
+
+int i;
+
+for (i=0; i<5; i++)
+{
+    printf("%d\n", arr_b[i]);
+}
+
+
+
+}
