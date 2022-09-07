3cb3123239d081e9448d52d9f847cafbc4527aaf
Ridel Samonte <ridel.asam@gmail.com>
2022-09-06 20:20:48 -0700

    complete class example

--- C_Cpp_Review/oop_style_cpp.cpp
+++ C_Cpp_Review/oop_style_cpp.cpp
@@ -1,44 +1,66 @@
 #include<iostream>
-#include<stdio.h>
 using namespace std;
-
-class Rectangle//class rectangle
+class Rectangle
 {
     private:
     int length;
-    int breath;
-
+    int breadth;
     public:
-
-    Rectangle ()//constructor of rectangle class ,it gets initalize as soon as class rectangle is declared in main 
+    Rectangle()//default constructor if no parameter is passed
     {
         length=0;
-        breath=05;
+        breadth=0;
     }
-
-    Rectangle (int l,int b)//constructor of rectangle class ,it gets initalize as soon as class rectangle is declared in main 
+    Rectangle(int l,int b);
+    int area();
+    int perimeter();//perimeter ,area,rectangle code could even be written inside class like getlength and others have
+    void setLength(int l)
     {
         length=l;
-        breath=b;
     }
-    int area()
+    void setbreadth(int b)
     {
-        return length*breath;
+        breadth=b;
     }
-    int peri()
+    int getLength()
     {
-        return 2*(length+breath);
-    }      
+        return length;
+    }
+    int getbreadth()
+    {
+        return breadth;
+    }
+    ~Rectangle()
+    {
+        cout<<"Destructor";
+    }
 };
-
+Rectangle::Rectangle(int l,int b)
+{
+    length=l;
+    breadth=b;
+}
+int Rectangle::area()
+{
+    return length*breadth;
+}
+int Rectangle::perimeter()
+{
+    return 2*(length+breadth);
+}
 int main()
 {
-    int l,b;
-    cout<<"Enter length and breath of rectangle ";
-    cin>>l>>b;
-    Rectangle r(l,b);
+    Rectangle r(10,4);
     int a=r.area();
-    int p=r.peri();
-    printf("%d is area\n%d is perimeter",a,p);
+    int p=r.perimeter();
+    printf("area = %d perimerter = %d\n",a,p);
+    r.setLength(5);
+    r.setbreadth(4);
+    a=r.area();
+    p=r.perimeter();
+    printf("area = %d perimerter = %d\n",a,p);
+    int l=r.getLength();
+    int b=r.getbreadth();
+    printf("Length = %d breadth = %d\n",l,b);
     return 0;
 }
