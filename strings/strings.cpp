780fb085f6e2f9124c6a6245ca95796159c73df9
Ridel Samonte <ridel.asam@gmail.com>
2022-09-13 09:13:36 -0700

    adding simple finding length function

--- strings/strings.cpp
+++ strings/strings.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 #include <stdio.h>
 
+
+void findlength (char s[]){
+    
+    int i = 0;
+    while(s[i]!='\0')
+    {
+        i++;
+    }
+    printf("length is %d", i);
+
+}
+
 int main()
 {
     char name1[] = "bruhhh";
@@ -8,5 +20,9 @@ int main()
     std::cout << "Enter your name ";
     gets(name2);
     printf("ok %s\n", name2);
-    std::cout << name1 << " and " << name2 << " are friends";
+    std::cout << name1 << " and " << name2 << " are friends\n";
+    
+    findlength(name2);
+    
+
 }
