07059cd905c9251755f7d9cf889983df924dfceb
Ridel Samonte <ridel.asam@gmail.com>
2022-09-13 09:30:27 -0700

    validating a string

---
+++ strings/validating_string.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+bool valid(char name[])
+{
+    for (int i = 0; name[i] != '\0'; i++)
+    {
+        if (!(name[i] >= 65 && name[i] <= 90) && !(name[i] >= 97 && name[i] <= 122) && !(name[i] >= 48 && name[i] <= 57))
+            return false;
+    }
+    return true;
+}
+int main()
+{
+    char name[] = "An3ura8?{}356HHHg";
+    std::cout << valid(name);
+    return 0;
+}
