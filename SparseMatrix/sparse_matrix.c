26e3ba5e67be36cce984b49a0f14d82b28b20170
Ridel Samonte <ridel.asam@gmail.com>
2022-09-15 09:52:47 -0700

    adding adding function

--- SparseMatrix/sparse_matrix.c
+++ SparseMatrix/sparse_matrix.c
@@ -14,6 +14,7 @@ struct Sparse
     int num;
     struct Element *ele;
 };
+
 void create(struct Sparse *s)
 {
     int i;
@@ -28,6 +29,7 @@ void create(struct Sparse *s)
     for (i = 0; i < s->num; i++)
         scanf("%d%d%d", &s->ele[i].i, &s->ele[i].j, &s-> ele[i].x);
 }
+
 void display(struct Sparse s)
 {
     int i, j, k = 0;
@@ -45,12 +47,61 @@ void display(struct Sparse s)
     }
 }
 
+struct Sparse *add(struct Sparse *s1, struct Sparse *s2)
+{
+    struct Sparse *sum;
+    int i, j, k;
+    i = j = k = 0;
+
+    if (s1->n != s2->n && s1->m != s2->m)
+        return NULL;
+    sum = (struct Sparse *)malloc(sizeof(struct Sparse));
+    sum->ele = (struct Element *)malloc((s1->num + s2->num) * sizeof(struct Element));
+    while (i < s1->num && j < s2->num)
+    {
+        if (s1->ele[i].i < s2->ele[j].i)
+            sum->ele[k++] = s1->ele[i++];
+        else if (s1->ele[i].i > s2->ele[j].i)
+            sum->ele[k++] = s2->ele[j++];
+        else
+        {
+            if (s1->ele[i].j < s2->ele[j].j)
+                sum->ele[k++] = s1->ele[i++];
+            else if (s1->ele[i].j > s2->ele[j].j)
+                sum->ele[k++] = s2->ele[j++];
+            else
+            {
+                sum->ele[k] = s1->ele[i];
+                sum->ele[k++].x = s1->ele[i++].x + s2->ele[j++].x;
+            }
+        }
+    }
+    for (; i < s1->num; i++)
+        sum->ele[k++] = s1->ele[i];
+    for (; j < s2->num; j++)
+        sum->ele[k++] = s2->ele[j];
+    sum->m = s1->m;
+    sum->n = s1->n;
+    sum->num = k;
+
+    return sum;
+}
+
 int main()
 {
-    struct Sparse s;
+    struct Sparse s1, s2, *s3;
 
-    create(&s);
-    display(s);
+    create(&s1);
+    create(&s2);
+
+    s3 = add(&s1, &s2);
+
+    printf("First Matrix\n");
+    display(s1);
+    printf("Second Matrix\n");
+    display(s2);
+    printf("Sum Matrix\n");
+    display(*s3);
 
     return 0;
 }
