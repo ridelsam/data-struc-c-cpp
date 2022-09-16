9c8d117de28f2dc1804792af3e49071b1b47739b
Ridel Samonte <ridel.asam@gmail.com>
2022-09-16 09:18:10 -0700

    including adding operator

--- SparseMatrix/polynomial_representation.cpp
+++ SparseMatrix/polynomial_representation.cpp
@@ -1,63 +1,78 @@
-#include <iostream>
+#include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-
-using namespace std;
-
-struct Node
+struct Term
 {
     int coeff;
     int exp;
-    struct Node *next;
-} *poly = NULL;
-
-void create()
+};
+struct Poly
 {
-    struct Node *t = NULL, *last = NULL;
-    int num;
-    cout << "Enter the number of polynomials ";
-    cin >> num;
+    int n;
+    struct Term *terms;
+};
+void create(struct Poly *p)
+{
+    int i;
+    printf("Number of terms?");
+    scanf("%d", &p->n);
+    p->terms = (struct Term *)malloc(p->n * sizeof(struct Term));
 
-    cout << "Enter the coefficient and exponent of each polynomial ";
-    for (int i = 0; i < num; i++)
+    printf("Enter terms\n");
+    for (i = 0; i < p->n; i++)
+        scanf("%d%d", &p->terms[i].coeff, &p -> terms[i].exp);
+}
+void display(struct Poly p)
+{
+    int i;
+    for (i = 0; i < p.n; i++)
+
+        printf("%dx%d+", p.terms[i].coeff, p.terms[i].exp);
+    printf("\n");
+}
+struct Poly *add(struct Poly *p1, struct Poly *p2)
+{
+    int i, j, k;
+    struct Poly *sum;
+
+    sum = (struct Poly *)malloc(sizeof(struct Poly));
+    sum->terms = (struct Term *)malloc((p1->n + p2 -> n) * sizeof(struct Term));
+    i = j = k = 0;
+
+    while (i < p1->n && j < p2->n)
     {
-        t = new Node;
-        cin >> t->coeff >> t->exp;
-        if (poly == NULL)
-        {
-            poly = last = t;
-        }
+        if (p1->terms[i].exp > p2->terms[j].exp)
+            sum->terms[k++] = p1->terms[i++];
+        else if (p1->terms[i].exp < p2->terms[j].exp)
+            sum->terms[k++] = p2->terms[j++];
         else
         {
-            last->next = t;
-            last = t;
+            sum->terms[k].exp = p1->terms[i].exp;
+            sum->terms[k++].coeff = p1->terms[i ++].coeff + p2->terms[j++].coeff;
         }
-        last->next = NULL;
     }
-}
+    for (; i < p1->n; i++)
+        sum->terms[k++] = p1->terms[i];
+    for (; j < p2->n; j++)
+        sum->terms[k++] = p2->terms[j];
 
-void display(struct Node *p)
-{
-    while (p)
-    {
-        cout << p->coeff << "x**" << p->exp << " + ";
-        p = p->next;
-    }
-}
-
-long eva(struct Node *p, int x)
-{
-    long val = 0;
-    while (p)
-    {
-        val += p->coeff * pow(x, p->exp);
-        p = p->next;
-    }
-    return val;
+    sum->n = k;
+    return sum;
 }
 int main()
 {
-    create();
-    display(poly);
-    cout << "\nAnswer of Evaluation" << eva(poly, 1);
+    struct Poly p1, p2, *p3;
+
+    create(&p1);
+    create(&p2);
+
+    p3 = add(&p1, &p2);
+
+    printf("\n");
+    display(p1);
+    printf("\n");
+    display(p2);
+    printf("\n");
+    display(*p3);
+
+    return 0;
 }
