91fc74ce795ce32f1944281c12f3d3ca1d8f2f73
Ridel Samonte <ridel.asam@gmail.com>
2022-09-30 21:22:05 -0700

    traversing tree iteratively

---
+++ trees/tree_traversal.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "includes/tree_func.cpp"
+using namespace std;
+ 
+
+int main() {
+ 
+    Tree bt;
+ 
+    bt.CreateTree();
+    cout << endl;
+ 
+    cout << "Preorder: " << flush;
+    bt.Preorder();
+    cout << endl;
+ 
+    cout << "Inorder: " << flush;
+    bt.Inorder();
+    cout << endl;
+ 
+    cout << "Postorder: " << flush;
+    bt.Postorder();
+    cout << endl;
+ 
+    cout << "Levelorder: " << flush;
+    bt.Levelorder();
+    cout << endl;
+ 
+    cout << "Height: " << bt.Height() << endl;
+ 
+    cout << "Iterative Preorder: " << flush;
+    bt.iterativePreorder();
+ 
+    cout << "Iterative Inorder: " << flush;
+    bt.iterativeInorder();
+ 
+    cout << "Iterative Postorder: " << flush;
+    bt.iterativePostorder();
+ 
+    return 0;
+}
