7825bba258701433fa284e359d4fbb63a0ba5802
Ridel Samonte <ridel.asam@gmail.com>
2022-10-03 19:34:44 -0700

    binary search tree program

---
+++ binary_search_tree/binary_search_tree.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "includes/bst_func.cpp"
+
+int main()
+{
+
+    BST bst;
+
+    // Insert
+    bst.Insert(10);
+    bst.Insert(5);
+    bst.Insert(20);
+    bst.Insert(8);
+    bst.Insert(30);
+
+    // Inorder traversal
+    bst.Inorder(bst.getRoot());
+    cout << endl;
+
+    // Search
+    Node *temp = bst.Search(2);
+    if (temp != nullptr)
+    {
+        cout << temp->data << endl;
+    }
+    else
+    {
+        cout << "Element not found" << endl;
+    }
+
+    return 0;
+}
